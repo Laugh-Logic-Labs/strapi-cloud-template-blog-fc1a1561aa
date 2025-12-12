593725a440fa6b491dee450fe66593513b6d9e33
myagnik-l3 <yagnik.laughlogiclabs@gmail.com>
2025-12-12 17:09:03 +0530

    feat: add new 'Status' content type and generate corresponding TypeScript types.

--- src/api/status/controllers/status.js
+++ src/api/status/controllers/status.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  category controller
+ * status controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::category.category');
+module.exports = createCoreController('api::status.status');
